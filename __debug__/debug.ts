4a07ffc33b07026e744260f88f158713102dc29d
Göran Gustafsson <dev@gorgus.se>
2016-02-21 20:45:56 +0100

    Change browser test code

--- __debug__/debug.ts
+++ __debug__/debug.ts
@@ -2,10 +2,10 @@ import { Promise } from 'es6-promise';
 
 import router, { RouterBrowserHistory, RouterConfigMap } from '../src/index';
 
-router.addConfig('app', {
+router.addConfig('a', {
 	url: '/',
 	data: {
-		level: 'root'
+		level: 'first'
 	},
 	errorPath: 'app.error',
 	configs: {
@@ -14,44 +14,54 @@ router.addConfig('app', {
 				error: true
 			}
 		},
-		login: {
-			url: '/login',
+		b1: {
+			url: '/b1',
 			data: {
-				mode: 'public'
-			}
-		},
-		maint: {
-			url: '/maint',
-			data: {
-				level: 'first',
-				mode: 'user'
+				level: 'second'
 			},
 			unrouted: true,
 			configs: {
-				party: {
-					url: '/party',
-					configs: {
-						detail: {
-							url: '/:partyId',
-							configs: {
-								edit: {
-								}
-							}
-						}
+				c1: {
+					url: '/c1',
+					data: {
+						level: 'third'
+					}
+				},
+				c2: {
+					url: '/c2',
+					data: {
+						level: 'third'
+					}
+				},
+				c3: {
+					data: {
+						level: 'third'
+					},
+				},
+				c4: {
+					url: '/c4/:arg1',
+					data: {
+						level: 'third'
 					}
 				}
 			}
 		},
-		history: {
-			url: '/history',
+		b2: {
+			url: '/b2',
 			unrouted: true,
+			data: {
+				level: 'second'
+			},
 			routeExtensionCallback: function(): Thenable<RouterConfigMap> {
 				console.log('Extend!');
 				return new Promise<RouterConfigMap>((resolve, reject) => {
 					setTimeout(() => {
 						resolve({
-							search: {
-								url: '/search'
+							c1: {
+								url: '/c1',
+								data: {
+									level: 'third'
+								}
 							}
 						});
 					}, 5000);
@@ -63,12 +73,12 @@ router.addConfig('app', {
 
 var msgElem: any = document.getElementById('content');
 var msgProp = ('innerText' in msgElem) ? 'innerText' : 'textContent';
-console.log(router.rootConfig);
-var hist: any = new RouterBrowserHistory('', true, !!(<any>window).useIFrameState, 'historyState');
+var hist: any = new RouterBrowserHistory('', true, false /*!!(<any>window).useIFrameState*/, 'historyState');
 router.start(hist, (state) => {
 	console.log('Found State');
 	console.log(state);
 	msgElem[msgProp] = JSON.stringify(state);
+	document.title = 'Test ' + state.historyTrackId;
 }, (path, url, partialState, error) => {
 	console.log('NotFound: ' + path + ', ' + url);
 	msgElem[msgProp] = 'NOT FOUND: ' + JSON.stringify(partialState);
@@ -84,37 +94,73 @@ router.start(hist, (state) => {
 	console.log('Transition End: ' + transitionId);
 });
 
-var elem = document.getElementById('partyEdit');
+
+var elem = document.getElementById('b1c1');
 if(elem.addEventListener) {
 	elem.addEventListener('click', (event) => {
 		event.preventDefault();
-		router.navigateTo('app.maint.party.detail.edit', { 'partyId': '1' }, {});
+		router.navigateTo('a.b1.c1', {}, {});
 	});
 } else {
 	(<any>elem).attachEvent('onclick', (event: any) => {
 		event.returnValue = false;
-		router.navigateTo('app.maint.party.detail.edit', { 'partyId': '1' }, {});
+		router.navigateTo('a.b1.c1', {}, {});
+		return false;
 	});	
 }
 
-var elem = document.getElementById('maintNav');
+var elem = document.getElementById('b1c2');
 if(elem.addEventListener) {
 	elem.addEventListener('click', (event) => {
 		event.preventDefault();
-		router.navigateTo('app.maint', {}, {});
+		router.navigateTo('a.b1.c2', {}, {});
 	});
 } else {
 	(<any>elem).attachEvent('onclick', (event: any) => {
 		event.returnValue = false;
-		router.navigateTo('app.maint', {}, {});
+		router.navigateTo('a.b1.c2', {}, {});
+		return false;
 	});	
 }
 
-Promise.resolve('hello!').then((value) => {
-	console.log('Value 1 = ' + value);
-	if(value === 'hello') {
-		return 'world';
-	}
-}).then((value2) => {
-	console.log('Value 2 = ' + value2);
-});
+var elem = document.getElementById('b1c3');
+if(elem.addEventListener) {
+	elem.addEventListener('click', (event) => {
+		event.preventDefault();
+		router.navigateTo('a.b1.c3', {}, {});
+	});
+} else {
+	(<any>elem).attachEvent('onclick', (event: any) => {
+		event.returnValue = false;
+		router.navigateTo('a.b1.c3', {}, {});
+		return false;
+	});	
+}
+
+var elem = document.getElementById('b1c4');
+if(elem.addEventListener) {
+	elem.addEventListener('click', (event) => {
+		event.preventDefault();
+		router.navigateTo('a.b1.c4', { arg1: 'xyz' }, {});
+	});
+} else {
+	(<any>elem).attachEvent('onclick', (event: any) => {
+		event.returnValue = false;
+		router.navigateTo('a.b1.c4', { arg1: 'xyz' }, {});
+		return false;
+	});	
+}
+
+var elem = document.getElementById('b2c1');
+if(elem.addEventListener) {
+	elem.addEventListener('click', (event) => {
+		event.preventDefault();
+		router.navigateTo('a.b2.c1', {}, {});
+	});
+} else {
+	(<any>elem).attachEvent('onclick', (event: any) => {
+		event.returnValue = false;
+		router.navigateTo('a.b2.c1', {}, {});
+		return false;
+	});	
+}
