2a48593c325ad0afdd4faea007debd3acc312185
Göran Gustafsson <dev@gorgus.se>
2016-02-23 22:52:54 +0100

    More unit tests for the router class

--- __tests__/router-test.ts
+++ __tests__/router-test.ts
@@ -2,17 +2,26 @@
 
 jest.dontMock('../src/router');
 jest.dontMock('../src/router-exception');
+jest.dontMock('../src/router-not-found-exception');
 jest.dontMock('extend');
 jest.dontMock('path-to-regexp');
+jest.dontMock('es6-promise');
+jest.dontMock('query-string');
+jest.dontMock('strict-uri-encode');
 
 import { Router } from '../src/router';
 import { RouterConfig } from '../src/router-types';
 import { RouterException } from '../src/router-exception';
+import { RouterNotFoundException } from '../src/router-not-found-exception';
 import { RouterMemoryHistory } from '../src/router-memory-history';
 
 class TestRouter extends Router {
 	getRootConfig(): RouterConfig {
 		return this.rootConfig;
+	}
+	
+	triggerUpdateUrl() {
+		this.updateUrl();
 	}
 }
 
@@ -101,6 +110,138 @@ describe('Router', function() {
 			expect((<any>history.stopHistoryUpdates).mock.calls.length).toBe(1);
 		});
 		
+		it('can be called even if the router is not running', function() {
+			router.start(history, () => {});
+			router.stop();
+			router.stop();
+			expect((<any>history.stopHistoryUpdates).mock.calls.length).toBe(1);
+		})
 	});
 	
+	describe('navigateTo', function() {
+		var history: RouterMemoryHistory;
+		
+		beforeEach(function() {
+			router = new TestRouter();
+			router.addConfig('a', {
+				url: '/a',
+				configs: {
+					'b1': {
+						url: '/b1'
+					},
+					'b2': {
+						url: '/b2',
+						configs: {
+							'c1': {
+								url: '/:uArg'
+							}
+						}
+					},
+					'b3': {
+						url: '/b3',
+						unrouted: true,
+						configs: {
+							'c1': {
+								url: '/c1',
+								reloadable: true
+							}
+						}
+					}
+				}
+			});
+			history = new RouterMemoryHistory();
+		});
+		
+		it('calls route found callback', function(done) {
+			router.start(history, (routerState) => {
+				expect(routerState.configPath).toBe('a');
+				done();
+			});
+			router.navigateTo('a');
+			jest.runAllTimers();
+		});
+		
+		it('calls route not found callback', function(done) {
+			router.start(history, (routerState) => {}, (configPath: string, fullUrl: string, matchedConfigs: RouterConfig[], error: any) => {
+				expect(configPath).toBe('z');
+				done();
+			});
+			router.navigateTo('z');
+			jest.runAllTimers();
+		});
+
+		it('calls transition begin callback', function(done) {
+			router.start(history, (routerState) => {}, () => {}, () => {}, (transitionId: number) => {
+				expect(transitionId).toBe(1);
+				done();
+			});
+			router.navigateTo('a');
+			jest.runAllTimers();
+		});
+
+		it('calls transition end callback', function(done) {
+			router.start(history, (routerState) => {}, () => {}, () => {}, () => {}, () => {}, (transitionId: number) => {
+				expect(transitionId).toBe(1);
+				done();
+			});
+			router.navigateTo('a');
+			jest.runAllTimers();
+		});
+		
+		it('calls transition cancel callback', function(done) {
+			router.start(history, (routerState) => {}, () => {}, () => {}, () => {}, (transitionId: number) => {
+				expect(transitionId).toBe(1);
+				done();
+			});
+			router.navigateTo('a');
+			router.navigateTo('a.b1');
+			jest.runAllTimers();
+		});
+		
+		it('applies url parameters', function(done) {
+			router.start(history, (routerState) => {
+				expect(routerState.urlParams).toEqual({ uArg: 'uValue' });
+				expect((<any>history.navigateTo).mock.calls.length).toBe(1);
+				expect((<any>history.navigateTo).mock.calls[0]).toEqual(['a.b2.c1', '/a/b2/uValue']);
+				done();
+			});
+			router.navigateTo('a.b2.c1', { uArg: 'uValue' });
+			jest.runAllTimers();
+		});
+		
+		it('applies query parameters', function(done) {
+			router.start(history, (routerState) => {
+				expect(routerState.queryParams).toEqual({ qArg: 'qValue' });
+				expect((<any>history.navigateTo).mock.calls.length).toBe(1);
+				expect((<any>history.navigateTo).mock.calls[0]).toEqual(['a', '/a?qArg=qValue']);
+				done();
+			});
+			router.navigateTo('a', {}, { qArg: 'qValue' });
+			jest.runAllTimers();
+		});
+		
+		it('can only be called if the router is running', function() {
+			expect(router.navigateTo('a')).toThrow(RouterException);
+		});
+		
+		it('will not navigate to unrouted states', function(done) {
+			router.start(history, (routerState) => {}, (configPath: string, fullUrl: string, matchedConfigs: RouterConfig[], error: any) => {
+				expect(configPath).toBe('a.b3');
+				expect(error instanceof RouterNotFoundException).toBeTruthy();
+				done();
+			});
+			router.navigateTo('a.b3');
+			jest.runAllTimers();
+		});
+		
+		it('will ask history to reload when navigating to reloadable state', function(done) {
+			router.start(history, (routerState) => {
+				expect((<any>history.reloadAtUrl).mock.calls.length).toBe(1);
+				done();
+			});
+			router.requestReload();
+			router.navigateTo('a.b3.c1');
+			jest.runAllTimers();
+		});
+	});
 });
